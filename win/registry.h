1cb8e0ed48e885e3e0e5a42c7b0023f9ed78ce3a
Eren Okka <erenokka@gmail.com>
2019-04-03 14:58:03 +0300

    Add missing headers
    Fixes compilation issues in VS2019.

--- win/registry.h
+++ win/registry.h
@@ -24,6 +24,7 @@ SOFTWARE.
 
 #pragma once
 
+#include <string>
 #include <vector>
 
 #include <windows.h>
--- win/task_dialog.cpp
+++ win/task_dialog.cpp
@@ -22,6 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+#include <string>
 #include <vector>
 
 #include "task_dialog.h"
